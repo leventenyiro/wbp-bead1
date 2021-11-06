22aec7d8eaf3d1689415a7b5ec8e7256949c630c
Levente Nyírő <nyiro.levente@gmail.com>
2021-11-06 07:42:38 +0100

    add divs

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let game = document.querySelector('#game')
+// rotations
+const UP = 0
+const RIGHT = 1
+const DOWN = 2
+const LEFT = 3
+// szobák
+const LINE = 0
+const CORNER = 1
+const T = 2
 
 // kezdőképernyő - új játék, leírás
 function startScreen() {
@@ -72,22 +81,47 @@ function startGame() {
     // div, jó sok divvel
     game.innerHTML = ""
     const board = document.createElement('div')
+    board.id = 'board'
     // ide jönnek majd a panelek
-    game.appendChild(board)
 
-    /*for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 7; i++) {
         const row = document.createElement('div')
+        row.classList.add('row')
         for (let j = 0; j < 7; j++) {
             const div = document.createElement('div')
-            div.style.backgroundColor = 'red'
+            //div.style.backgroundColor = 'red'
             row.appendChild(div)
         }
-        game.appendChild(row)
-    }*/
+        board.appendChild(row)
+    }
+    game.appendChild(board)
 
-    state.board = new Room(0, 0, CORNER, rotation)
+    //board = new Room(0, 0, CORNER, rotation)
 }
 
+const starterBoard = () => {
+    return [
+        new Room(0, 0, CORNER, UP),
+        new Room(0, 6, CORNER, RIGHT),
+        new Room(6, 6, CORNER, DOWN),
+        new Room(6, 0, CORNER, LEFT),
 
+        new Room(2, 0, T, UP),
+        new Room(4, 0, T, UP),
+
+        new Room(2, 2, T, LEFT),
+        new Room(4, 2, T, LEFT),
+        new Room(6, 2, T, UP),
+        new Room(8, 2, T, RIGHT),
+
+        new Room(2, 4, T, LEFT),
+        new Room(4, 4, T, DOWN),
+        new Room(6, 4, T, RIGHT),
+        new Room(8, 4, T, RIGHT),
+
+        new Room(4, 8, T, DOWN),
+        new Room(6, 8, T, DOWN),
+    ]
+}
 
 startScreen()
